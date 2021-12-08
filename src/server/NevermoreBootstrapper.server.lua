4f3a387777d261bf4a78571aa515a287234494f9
cassis163 <cas.aangeenbrug@gmail.com>
2021-12-08 22:57:28 +0100

    Add NevermoreEngine modules

---
+++ src/server/NevermoreBootstrapper.server.lua
@@ -0,0 +1,7 @@
+local ServerScriptService = game:GetService("ServerScriptService")
+
+local NevermoreModules = ServerScriptService:WaitForChild("Nevermore")
+local Loader = require(NevermoreModules:FindFirstChild("loader"))
+
+Loader.bootstrapGame(NevermoreModules)
+NevermoreModules:Destroy()
