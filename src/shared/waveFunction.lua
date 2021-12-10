f508bc9a3e7ba78a1ed10a3df158099d7f9beba6
cassis163 <cas.aangeenbrug@gmail.com>
2021-12-10 17:45:14 +0100

    Change magic numbers to constants

--- src/shared/waveFunction.lua
+++ src/shared/waveFunction.lua
@@ -1,7 +1,10 @@
+local SCALE = 30
+local BIAS = 100
+
 return function (p, t)
 	return Vector3.new(
 		p.X,
-		math.sin(p.X + t) + math.cos(p.Z + t) * 20,
+		math.sin(p.X / SCALE + t) * math.cos(p.Z / SCALE + t) + BIAS,
 		p.Z
 	)
 end
