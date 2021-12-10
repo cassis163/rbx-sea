025c251a0c78e27aea8a37049be630f8682f5452
cassis163 <cas.aangeenbrug@gmail.com>
2021-12-10 19:52:22 +0100

    Change waveFunction

--- src/shared/waveFunction.lua
+++ src/shared/waveFunction.lua
@@ -1,10 +1,11 @@
-local SCALE = 30
-local BIAS = 100
+local AMPLITUDE = 20
+local SCALE = 100
+local BIAS = 50
 
 return function (p, t)
 	return Vector3.new(
 		p.X,
-		math.sin(p.X / SCALE + t) * math.cos(p.Z / SCALE + t) + BIAS,
+		AMPLITUDE * math.sin(p.X / SCALE + t) * math.cos(p.Z / SCALE + t) + BIAS,
 		p.Z
 	)
 end
