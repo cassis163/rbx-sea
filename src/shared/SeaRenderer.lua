4067878292acf6d60a5244dd094ba4328ed04676
cassis163 <cas.aangeenbrug@gmail.com>
2021-12-11 13:38:36 +0100

    Add texture offsets

--- src/shared/SeaRenderer.lua
+++ src/shared/SeaRenderer.lua
@@ -1,31 +1,16 @@
+local INITIAL_RADIUS = 125
+local TEXTURE_STRETCH_FIX = 8
+
 local scaleMesh = require(script.Parent:WaitForChild("scaleMesh"))
 
 local SeaRenderer = {}
 SeaRenderer.__index = SeaRenderer
 
-local INITIAL_RADIUS = 125
-
-function SeaRenderer:Update(x, y, t)
-    self:_Reposition(x, y)
-
-	for _, bone in pairs(self._bones) do
-		bone.WorldPosition = self._waveFunction(bone.WorldPosition, t)
-	end
-end
-
-function SeaRenderer:_Init()
-    self:_Normalize()
-    self:_Resize(INITIAL_RADIUS)
-    self:_TransformToCircle()
-    self:_DisplaceBones()
-    -- self:_Test()
-	self._mesh.Parent = self._parent
-end
-
 function SeaRenderer.new(mesh, waveFunction, parent, distanceFactor)
 	local obj = setmetatable({}, SeaRenderer)
 	obj._mesh = mesh
 	obj._meshPlane = mesh:WaitForChild("Plane")
+    obj._texture = obj._meshPlane:WaitForChild("Texture")
 	obj._waveFunction = waveFunction
 	obj._parent = parent
 	obj._distanceFactor = distanceFactor or 0.1
@@ -33,6 +18,33 @@ function SeaRenderer.new(mesh, waveFunction, parent, distanceFactor)
 	obj:_Init()
 
 	return obj
+end
+
+function SeaRenderer:Update(x, y, t)
+    self:_Reposition(x, y)
+    self:_UpdateTexture(x, y)
+    self:_RecalculateBonePositions(t)
+end
+
+function SeaRenderer:_Init()
+    self:_Normalize()
+    self:_Resize(INITIAL_RADIUS)
+    self:_DisplaceBones()
+    -- self:_Test()
+
+	self._mesh.Parent = self._parent
+end
+
+function SeaRenderer:_RecalculateBonePositions(t)
+    for _, bone in pairs(self._bones) do
+		bone.WorldPosition = self._waveFunction(bone.WorldPosition, t)
+	end
+end
+
+function SeaRenderer:_UpdateTexture(x, y)
+    -- This should be -x and -y in theory, but the textures are stretched
+    self._texture.OffsetStudsU = -x / TEXTURE_STRETCH_FIX
+    self._texture.OffsetStudsV = -y / TEXTURE_STRETCH_FIX
 end
 
 function SeaRenderer:_Reposition(x, y)
@@ -57,28 +69,6 @@ end
 function SeaRenderer:_Normalize()
     -- Normalize the mesh to 1x1
 	scaleMesh(self._meshPlane, Vector3.new(1, 1, 1) / self._meshPlane.Size)
-end
-
-function SeaRenderer:_TransformToCircle()
-    -- This is essentially a triangle wave with a reach of [1, sqrt(2)] with a period of pi
-    local function getDistanceRatio(theta)
-        local t = theta
-        local p = math.pi
-        local a = math.sqrt(2) - 1
-
-        -- Inspired by https://www.calculushowto.com/triangle-wave-function/https://www.calculushowto.com/triangle-wave-function/
-        return a * math.abs(4 / p * (t - p / 2 * math.floor(2 * t / p + 0.5 )) * (-1)^math.floor(2 * t / p + 0.5)) + 1
-    end
-
-    for _, bone in pairs(self._bones) do
-        local bX = bone.Position.X
-        local bY = bone.Position.Z
-        local theta = math.atan2(bY, bX)
-        local k = getDistanceRatio(theta)
-
-        -- Reposition the bones to transform the mesh from rectangular to somewhat circular
-        bone.Position /= Vector3.new(k, 1, k)
-    end
 end
 
 function SeaRenderer:_DisplaceBones()
