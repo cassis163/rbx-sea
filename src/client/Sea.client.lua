b12970774243e910abdefcc57d2d9645415b2fc9
cassis163 <cas.aangeenbrug@gmail.com>
2021-12-12 19:16:21 +0100

    Fix index nil error

--- src/client/Sea.client.lua
+++ src/client/Sea.client.lua
@@ -15,17 +15,23 @@ local SyncedClock = TimeSyncService:GetSyncedClock()
 local SeaInstance = Sea.new(19, 50, waveFunction)
 
 local function update(t)
-    local function getTargetInstance()
-        local character = Player.Character
-        if character then
-            local head = character:FindFirstChild("Head")
+    local function getPosition()
+        local function getTargetInstance()
+            local character = Player.Character
+            if character then
+                local head = character:FindFirstChild("Head")
 
-            return head and head or workspace.CurrentCamera
+                return head and head or workspace.CurrentCamera
+            end
         end
+
+        local targetInstance = getTargetInstance()
+
+        return targetInstance and targetInstance.CFrame or Vector3.new()
     end
 
-    local TargetInstance = getTargetInstance()
-    SeaInstance:Update(TargetInstance.CFrame.X, TargetInstance.CFrame.Z, t)
+    local position = getPosition()
+    SeaInstance:Update(position.X, position.Z, t)
 end
 
 local t = tick()
